2e851712a7bf67b6de40af33acda4d0556a55380
Justyna Poturała <j.poturala@wp.pl>
2020-08-19 17:13:51 +0200

    Fixed header

--- script.js
+++ script.js
@@ -8,3 +8,24 @@ burger.addEventListener("click", function () {
     }
     activeNav.classList.toggle("on");
 })
+
+const headerElement = document.querySelector('header');
+const headerContainer = document.querySelector('.header-container');
+const navigation = document.querySelector('.activeNav');
+// const images = document.querySelectorAll('.imgBox img');
+const logoImage = document.querySelector('.logo');
+
+window.addEventListener("scroll",
+    function () {
+        headerElement.classList.add("fixed");
+        headerContainer.classList.add("fixed");
+        navigation.classList.add("fixed");
+        logoImage.classList.add("fixed");
+    }
+)
+
+// burger.addEventListener("click", function () {
+//     images.forEach(function (img) {
+//         img.classList.toggle("fixed");
+//     })
+// })
